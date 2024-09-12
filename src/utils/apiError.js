b4884bc67ee207b259c88a661e9287d6910a4c33
Raj Dutta <duttabipul927@gmail.com>
2024-09-12 21:45:50 +0530

    configure cloudinary in our backend

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,5 +1,5 @@
 class ApiError extends Error {
-    constructor(statusCode, message="Sonething went wrong", errors=[], statck="") {
+    constructor(statusCode, message="Sonething went wrong", errors=[], stack="") {
         super(message);
         this.statusCode = statusCode;
         this.data = null;
@@ -7,8 +7,8 @@ class ApiError extends Error {
         this.sucess = false;
         this.errors = errors;
 
-        if (statck) {
-            this.stack = statck    
+        if (stack) {
+            this.stack = stack    
         } else {
             Error.captureStackTrace(this, this.constructor)
         }
